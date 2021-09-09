55f531c896210faef080db42bdc635bce2ed08fe
Michael Robards <mikerobards@icloud.com>
2021-09-09 16:56:07 -0400

    add ProductDetail component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, Link } from "react-router-dom"
 
 import Home from "./Home";
 import Products from "./Products";
+import ProductDetail from "./ProductDetail";
 
 import './App.css';
 
@@ -18,8 +19,11 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/products">
+        <Route exact path="/products">
           <Products />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetail />
         </Route>
       </Switch>
     </div>
--- src/Products.js
+++ src/Products.js
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom"
 
 function Products() {
     const products = productsData.map(product => (
-        <h3 key={product.id}>
-            <Link to={`/products/${product.id}`}>{product.name}</Link>
-            <br />
-            {product.description}
-            <br />
-            ${product.price}
-        </h3>
+        <div key={product.id}>
+            <h3>
+                <Link to={`/products/${product.id}`}>{product.name}</Link>
+            </h3>
+            <p>Price: ${product.price}</p>
+            <hr />
+        </div>
     ))
     return (
-        <div>
+        <>
             <h1>Products Page</h1>
             {products}
-        </div>
+        </>
     )
 }
 
